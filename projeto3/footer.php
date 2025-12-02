50522e5cdfc9c4fabf634cc77a297e97dcc02fec
Ghost_Hunter <eg1681667@gmail.com>
2025-12-02 09:14:59 -0300

    Add files via upload

---
+++ projeto3/footer.php
@@ -0,0 +1,28 @@
+<html>
+
+<body>
+<link
+     rel="stylesheet"
+     
+     href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+ >
+ <script
+  src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js">
+</script>
+      
+<footer class="bg-light text-center text-lg-start mt-5">
+  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+    © cadastre e garanta beneficios:
+    <a class="text-dark" href="
+</footer>
+<div class="card" style="width: 20rem;">
+ <img src="https://fdr.com.br/wp-content/uploads/2022/04/cadastro-unico-federal-fdr-4-750x406.jpg" class="card-img-top" alt="...">
+<div class="card-body">
+    <h5 class="card-title">PESSOA CADASTRADA</h5>
+     <h6>Para verificar as pessoas cadrastada clique aqui em baixo </h6>
+     <a href="/listar.php" class="card-link">Ver todos</a>
+  </div>
+</div>
+        
+</body>
+</html>
